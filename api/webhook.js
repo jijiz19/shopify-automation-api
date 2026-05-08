9cbc8f35658512c85778e98122222a2f474be0e5
jijiz19 <arboleda.jizryl@student.capsu.edu.ph>
2026-05-08 11:00:49 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     // ----------------------------
     // SEND TO GOOGLE APPS SCRIPT
     // ----------------------------
-    await fetch("YOUR_GOOGLE_APPS_SCRIPT_WEBAPP_URL", {
+    await fetch("https://script.google.com/macros/s/AKfycbyzMghOaW_Vl59pvDBdz2bqsb4bWb3uADRwe5gL8-5Kl4om1jcHNMgj7wlTvsHkvluttQ/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
